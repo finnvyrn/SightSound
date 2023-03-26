e5d866f850adc35812c58dec749dd0bf24344d76
Ary <ary.dinesen@icloud.com>
2023-03-27 05:12:04 +0800

    Initial Commit

---
+++ SightSound/SightSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightSoundApp.swift
+//  SightSound
+//
+//  Created by Ethan Quinn on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SightSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
