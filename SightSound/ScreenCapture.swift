ba297667d175b2aa769634fadaf36bed64b0e8a1
Ethan Quinn <ary.dinesen@icloud.com>
2023-03-28 03:56:44 +0800

    s

--- SightSound/ScreenCapture.swift
+++ SightSound/ScreenCapture.swift
@@ -6,22 +6,19 @@
 
 func captureScreen() -> CGImage? {
   #if os(iOS)
-    guard let window = UIApplication.shared.windows.first else {
-      return nil
-    }
-
-    let renderer = UIGraphicsImageRenderer(bounds: window.bounds)
+    guard let window = UIApplication.shared.windows.first else { return nil }
+    let renderer = UIGraphicsImageRenderer(size: window.bounds.size)
     let image = renderer.image { context in
       window.drawHierarchy(in: window.bounds, afterScreenUpdates: true)
     }
-
     return image.cgImage
   #elseif os(macOS)
-    guard let window = NSApp.windows.first else {
-      return nil
-    }
-
-    return window.contentView?.bitmapImageRepForCachingDisplay(in: window.contentView!.bounds)?
-      .cgImage
+    guard let window = NSApp.windows.first else { return nil }
+    guard let contentView = window.contentView else { return nil }
+    let size = contentView.bounds.size
+    let imageRepresentation = contentView.bitmapImageRepForCachingDisplay(in: contentView.bounds)
+    contentView.cacheDisplay(in: contentView.bounds, to: imageRepresentation!)
+    guard let cgImage = imageRepresentation?.cgImage else { return nil }
+    return cgImage
   #endif
 }
