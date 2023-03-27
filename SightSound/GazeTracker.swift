87f0de527a8ad57f2440f850a2565bc2e35d046b
Ethan Quinn <ary.dinesen@icloud.com>
2023-03-28 04:45:41 +0800

    s

--- SightSound/GazeTracker.swift
+++ SightSound/GazeTracker.swift
@@ -30,8 +30,14 @@ private func averagePoint(points: [CGPoint]) -> CGPoint {
 }
 
 private func gazePointInScreenCoordinates(gazePoint: CGPoint, imageSize: CGSize) -> CGPoint {
-  let screenWidth = UIScreen.main.bounds.size.width
-  let screenHeight = UIScreen.main.bounds.size.height
+
+  #if os(iOS)
+    let screenWidth = UIScreen.main.bounds.size.width
+    let screenHeight = UIScreen.main.bounds.size.height
+  #elseif os(macOS)
+    let screenWidth = NSScreen.main?.frame.size.width ?? 0
+    let screenHeight = NSScreen.main?.frame.size.height ?? 0
+  #endif
 
   let gazeX = gazePoint.x * screenWidth
   let gazeY = screenHeight - gazePoint.y * screenHeight
@@ -66,8 +72,21 @@ private func handleDetectedFace(request: VNRequest, error: Error?) {
 
 func detectText(at gazePoint: CGPoint) {
   // Capture a screenshot of the current screen
-  guard let screenShot = UIApplication.shared.windows.first?.snapshot() else {
-    print("Failed to capture a screenshot.")
+  #if os(iOS)
+    guard let screenShot = UIApplication.shared.windows.first?.snapshot() else {
+      print("Failed to capture a screenshot.")
+      return
+    }
+  #elseif os(macOS)
+    guard let screenShot = NSApplication.shared.keyWindow?.snapshot() else {
+      print("Failed to capture a screenshot.")
+      return
+    }
+  #endif
+
+  // Convert the NSImage to a CGImage
+  guard let cgImage = screenShot.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+    print("Failed to convert NSImage to CGImage.")
     return
   }
 
@@ -78,11 +97,11 @@ func detectText(at gazePoint: CGPoint) {
       return
     }
 
+    let distanceThreshold: CGFloat = 50.0  // Adjust this value to control the text detection sensitivity
+
     for textObservation in results {
       let boundingBox = textObservation.boundingBox
       let textCenter = CGPoint(x: boundingBox.midX, y: boundingBox.midY)
-
-      let distanceThreshold: CGFloat = 50.0  // Adjust this value to control the text detection sensitivity
 
       // Check if the gaze point is close enough to the text center
       if abs(gazePoint.x - textCenter.x) <= distanceThreshold
@@ -90,26 +109,21 @@ func detectText(at gazePoint: CGPoint) {
       {
         // Speak the detected text
         print("Text detected at gaze point: \(textObservation)")
+
+        // Replace this example text with the actual text you want to speak
+        let exampleText = "Detected text at gaze point."
+        SpeechSynthesizer.shared.speakText(exampleText)
       }
     }
   }
+}
 
-  let handler = VNImageRequestHandler(cgImage: screenShot.cgImage!, options: [:])
-
-  do {
-    try handler.perform([textDetectionRequest])
-  } catch {
-    print("Failed to perform text detection: (error)")
-  }
-
-  if abs(gazePoint.x - textCenter.x) <= distanceThreshold
-    && abs(gazePoint.y - textCenter.y) <= distanceThreshold
-  {
-    // Speak the detected text
-    print("Text detected at gaze point: \(textObservation)")
-
-    // Replace this example text with the actual text you want to speak
-    let exampleText = "Detected text at gaze point."
-    SpeechSynthesizer.shared.speakText(exampleText)
+extension NSWindow {
+  func snapshot() -> NSImage? {
+    guard let contentView = contentView else { return nil }
+    let bounds = contentView.bounds
+    let bitmapRep = contentView.bitmapImageRepForCachingDisplay(in: bounds)
+    contentView.cacheDisplay(in: bounds, to: bitmapRep!)
+    return NSImage(cgImage: bitmapRep!.cgImage!, size: bounds.size)
   }
 }
